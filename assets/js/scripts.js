e79712d2e5de801b1497c887c321db23244f6a53
Akshay Kamath <a.kamath@hzdr.de>
2024-11-20 12:54:58 +0100

    Smoother Scroll

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,17 +7,16 @@ window.addEventListener('scroll', () => {
 
     if (scrollY > 50 && !scrolled) {
         navbar.classList.add('show');
-        heroTitle.style.transform = 'translate(-50%, -50%) scale(0.5)';
-        heroTitle.style.position = 'fixed';
         heroTitle.style.top = '1rem';
         heroTitle.style.left = '2rem';
-        heroTitle.style.transformOrigin = 'left top';
+        heroTitle.style.transform = 'translate(0, 0) scale(0.5)';
         heroTitle.style.color = '#007aff';
         scrolled = true;
     } else if (scrollY <= 50 && scrolled) {
         navbar.classList.remove('show');
+        heroTitle.style.top = '50%';
+        heroTitle.style.left = '50%';
         heroTitle.style.transform = 'translate(-50%, -50%) scale(1)';
-        heroTitle.style.position = 'static';
         heroTitle.style.color = '#f5f5f7';
         scrolled = false;
     }
